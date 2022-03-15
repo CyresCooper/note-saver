2fd579d82fa3fd60cd9aa42e4902996fd549ee54
Cyres Cooper <95892453+CyresCooper@users.noreply.github.com>
2022-03-15 18:00:51 -0400

    notes being saved and posted on local host

--- server.js
+++ server.js
@@ -4,3 +4,44 @@ const PORT = process.env.PORT || 4001;
 const fs = require('fs');
 const path = require('path');
 const allNotes = require('./db/db.json');
+
+//linking 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//get functions 
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.post("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+      if (error) {
+        return console.log(error)
+      }
+      notes = JSON.parse(notes)
+      let id = notes[notes.length - 1].id + 1
+      let newNote = { title: req.body.title, text: req.body.text, id: id }
+      let activeNote = notes.concat(newNote)
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+        if (error) {
+          return error
+        }
+        console.log(activeNote)
+        res.json(activeNote);
+      })
+    })
+  })
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
